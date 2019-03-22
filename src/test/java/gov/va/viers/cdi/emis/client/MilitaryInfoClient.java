87c5ce4153bc1da2a48ec28f9d512bfac4701a7f
Evan Clendenning <evan.clendenning@libertyits.com>
2019-03-22 12:24:59 -0400

    cleaned up code, made variable names more clear

--- src/test/java/gov/va/viers/cdi/emis/client/MilitaryInfoClient.java
+++ src/test/java/gov/va/viers/cdi/emis/client/MilitaryInfoClient.java
@@ -5,11 +5,9 @@ import gov.va.viers.cdi.emis.commonservice.v2.InputEdipiIcn;
 import gov.va.viers.cdi.emis.requestresponse.militaryinfo.v2.ObjectFactory;
 import gov.va.viers.cdi.emis.requestresponse.v2.EMISmilitaryServiceEligibilityResponseType;
 import gov.va.viers.cdi.emis.requestresponse.v2.InputEdiPiOrIcn;
-import java.io.IOException;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Marshaller;
-import javax.xml.transform.TransformerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,17 +24,21 @@ public class MilitaryInfoClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(
         MilitaryInfoClient.class);
 
+  ObjectFactory requestFactory = new ObjectFactory();
+
+  gov.va.viers.cdi.cdi.commonservice.v2.ObjectFactory headerFactory =
+      new gov.va.viers.cdi.cdi.commonservice.v2.ObjectFactory();
+
     @Autowired
     private WebServiceTemplate webServiceTemplate;
 
     public JAXBElement<EMISmilitaryServiceEligibilityResponseType> getMilitaryServiceEligibilityResponse (String value, String type) {
-        ObjectFactory factory = new ObjectFactory();
         InputEdiPiOrIcn input = new InputEdiPiOrIcn();
         InputEdipiIcn edi = new InputEdipiIcn();
         edi.setEdipiORicnValue(value);
         edi.setInputType(type);
         input.setEdipiORicn(edi);
-        JAXBElement<InputEdiPiOrIcn> request = factory.createEMISmilitaryServiceEligibilityRequest(input);
+        JAXBElement<InputEdiPiOrIcn> request = requestFactory.createEMISmilitaryServiceEligibilityRequest(input);
 
     JAXBElement<EMISmilitaryServiceEligibilityResponseType> response =
         (JAXBElement<EMISmilitaryServiceEligibilityResponseType>)
@@ -51,23 +53,21 @@ public class MilitaryInfoClient {
                       SoapHeader soapHeader = ((SoapMessage) message).getSoapHeader();
 
                       // create the header element
-                      gov.va.viers.cdi.cdi.commonservice.v2.ObjectFactory objectFactory =
-                          new gov.va.viers.cdi.cdi.commonservice.v2.ObjectFactory();
 
-                      InputHeaderInfo header = objectFactory.createInputHeaderInfo();
+                      InputHeaderInfo header = headerFactory.createInputHeaderInfo();
                       header.setSourceSystemName("sourceSystem");
                       header.setUserId("userId");
                       header.setTransactionId("transactionId");
 
-                      JAXBElement<InputHeaderInfo> headers =
-                          objectFactory.createInputHeaderInfo(header);
+                      JAXBElement<InputHeaderInfo> jaxbHeader =
+                          headerFactory.createInputHeaderInfo(header);
 
                       // create a marshaller
                       JAXBContext context = JAXBContext.newInstance(InputHeaderInfo.class);
                       Marshaller marshaller = context.createMarshaller();
 
                       // marshal the headers into the specified result
-                      marshaller.marshal(headers, soapHeader.getResult());
+                      marshaller.marshal(jaxbHeader, soapHeader.getResult());
                     } catch (Exception e) {
                       LOGGER.error("error during marshalling of the SOAP headers", e);
                     }
