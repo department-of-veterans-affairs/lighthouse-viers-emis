02c77a93f14301c51fca46a852b99538dd28e25d
jtorano <joseph.torano@libertyits.com>
2019-04-11 15:36:14 -0400

    added getters and setters for ResponseTuple

--- src/main/java/gov/va/viers/cdi/emis/ws/endpoint/ResponseTuple.java
+++ src/main/java/gov/va/viers/cdi/emis/ws/endpoint/ResponseTuple.java
@@ -2,10 +2,25 @@ package gov.va.viers.cdi.emis.ws.endpoint;
 
 import gov.va.schema.emis.vdrdodadapter.v2.ESSErrorType;
 import javax.xml.bind.JAXBElement;
-import lombok.Data;
 
-@Data
 public class ResponseTuple {
+
   JAXBElement response;
   ESSErrorType error;
+
+  public JAXBElement getResponse() {
+    return response;
+  }
+
+  public void setResponse(JAXBElement response) {
+    this.response = response;
+  }
+
+  public ESSErrorType getError() {
+    return error;
+  }
+
+  public void setError(ESSErrorType error) {
+    this.error = error;
+  }
 }
