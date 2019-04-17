47c315b2887c3d8f598f666a73c7045966e384b1
Evan Clendenning <evan.clendenning@libertyits.com>
2019-04-17 16:01:15 -0400

    slim down mappers where there is a recursive one to one pattern

--- src/main/java/gov/va/emis/mappers/GuardReserveServicePeriodsMapper.java
+++ src/main/java/gov/va/emis/mappers/GuardReserveServicePeriodsMapper.java
@@ -1,11 +1,6 @@
 package gov.va.emis.mappers;
 
-import gov.va.viers.cdi.cdi.commonservice.v2.ESSErrorType;
-import gov.va.viers.cdi.emis.commonservice.v2.GuardReserveServicePeriods;
-import gov.va.viers.cdi.emis.commonservice.v2.GuardReserveServicePeriodsData;
-import gov.va.viers.cdi.emis.commonservice.v2.KeyData;
 import gov.va.viers.cdi.emis.requestresponse.v2.EMISguardReserveServicePeriodsResponseType;
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -18,19 +13,4 @@ public interface GuardReserveServicePeriodsMapper {
   EMISguardReserveServicePeriodsResponseType mapEMISguardReserveServicePeriodsResponseType(
       gov.va.schema.emis.vdrdodadapter.v2.EMISguardReserveServicePeriodsResponseType
           emiSguardReserveServicePeriodsResponseType);
-
-  List<GuardReserveServicePeriods> mapGuardReserveServicePeriodsList(
-      List<gov.va.schema.emis.vdrdodadapter.v2.GuardReserveServicePeriods>
-          guardReserveServicePeriodsList);
-
-  KeyData mapKeyData(gov.va.schema.emis.vdrdodadapter.v2.KeyData keyData);
-
-  GuardReserveServicePeriods mapGuardReserveServicePeriods(
-      gov.va.schema.emis.vdrdodadapter.v2.GuardReserveServicePeriods guardReserveServicePeriods);
-
-  GuardReserveServicePeriodsData mapGuardReserveServicePeriodsData(
-      gov.va.schema.emis.vdrdodadapter.v2.GuardReserveServicePeriodsData
-          guardReserveServicePeriodsData);
-
-  ESSErrorType mapEssErrorType(gov.va.schema.emis.vdrdodadapter.v2.ESSErrorType essErrorType);
 }
