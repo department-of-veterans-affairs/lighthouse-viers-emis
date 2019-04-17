867dd5d1607368143474891246c32f26e81b8c9e
Evan Clendenning <evan.clendenning@libertyits.com>
2019-04-17 16:59:44 -0400

    slimmed down military occupation mapper

--- src/main/java/gov/va/emis/mappers/MilitaryOccupationMapper.java
+++ src/main/java/gov/va/emis/mappers/MilitaryOccupationMapper.java
@@ -1,10 +1,6 @@
 package gov.va.emis.mappers;
 
-import gov.va.viers.cdi.emis.commonservice.v2.KeyData;
-import gov.va.viers.cdi.emis.commonservice.v2.MilitaryOccupation;
-import gov.va.viers.cdi.emis.commonservice.v2.MilitaryOccupationData;
 import gov.va.viers.cdi.emis.requestresponse.v2.EMISmilitaryOccupationResponseType;
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -16,12 +12,4 @@ public interface MilitaryOccupationMapper {
   EMISmilitaryOccupationResponseType mapEMISmilitaryOccupationResponseType(
       gov.va.schema.emis.vdrdodadapter.v2.EMISmilitaryOccupationResponseType
           emiSmilitaryOccupationResponseType);
-
-  List<MilitaryOccupation> mapMilitaryOccupationList(
-      List<gov.va.schema.emis.vdrdodadapter.v2.MilitaryOccupation> militaryOccupationList);
-
-  KeyData mapKeyData(gov.va.schema.emis.vdrdodadapter.v2.KeyData keyData);
-
-  MilitaryOccupationData mapMilitaryOccupationData(
-      gov.va.schema.emis.vdrdodadapter.v2.MilitaryOccupationData militaryOccupation);
 }
